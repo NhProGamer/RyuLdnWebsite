867eab02830f690c5f014dd84c8d7fb68a29f787
NhProGamer <neo.huyghe@gmail.com>
2024-11-07 22:21:06 +0100

    tiny fixes on icons alignment

--- static/main.js
+++ static/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         });
     }
 
-    $.getJSON("/api", function (data) {
+    $.getJSON("https://ryuldnweb2.vudjun.com/api", function (data) {
         $(".players-public").text(data.public_player_count);
         $(".players-private").text(data.private_player_count);
         $(".players-total").text(data.total_player_count);
@@ -18,7 +18,7 @@ $(document).ready(function () {
         $(".proxy-server-total").text(data.master_proxy_count);
     });
 
-    $.getJSON("/api/public_games", function (data) {
+    $.getJSON("https://ryuldnweb2.vudjun.com/api/public_games", function (data) {
         $(".public-games").empty();
         $.each(data, function () {
             $(".public-games").append(`
@@ -32,17 +32,18 @@ $(document).ready(function () {
                             <span class="bg-surface0 text-white px-2 py-1 rounded-md text-sm ml-2">${this.title_id}</span>
                             <span class="bg-surface0 text-white px-2 py-1 rounded-md text-sm ml-2">v${this.title_version}</span>
                             
-                            <svg viewBox="0 0 24 24" fill="currentColor" class="inline-block h-5 w-5 mr-2 text-accent ml-auto"><path fill-rule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clip-rule="evenodd" /><path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" /></svg>
+                            <svg class="inline-block h-6 w-6 text-accent_error ml-2 mr-1" viewBox="0 0 24 24" fill="currentColor"> <path fill-rule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clip-rule="evenodd" /><path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" /></svg>
                             <span>${this.player_count}/${this.max_player_count}</span>
                         </div>            
                     </div>
                     <div class="p-4">
                         <blockquote class="mb-0">
-                            <div class="flex items-center mb-2">
-                                <svg class="inline-block h-5 w-5 mr-2 text-accent" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" />
+                            <div class="flex items-center">
+                                <span>
+                                <svg class="inline-block h-5 w-5 text-accent" viewBox="0 0 24 24" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
                                 </svg>
-                                <span>${this.players.map((player) => encode(player)).join(', <svg class="inline-block h-5 w-5 mr-2 text-accent" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" /></svg> ')}</span>
+                                ${this.players.map((player) => encode(player)).join(' <svg class="inline-block h-5 w-5 text-accent" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" /></svg>')}</span>
                             </div>
                             <footer class="font-normal text-accent flex items-center">
                                 <svg class="inline-block h-5 w-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
